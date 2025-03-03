fac654927d6dcd9d98b16e15e27a5f891ba3992d
donnyslayzz <Donnyslayzz98@gmail.com>
2025-03-03 11:10:56 -0800

    Deploy frontend to GitHub Pages

---
+++ dist/js/724.52d5a5a3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcoinds_web"]=self["webpackChunkcoinds_web"]||[]).push([[724],{1343:function(n,e,u){u.r(e),u.d(e,{default:function(){return o}});var r=u(6768);function t(n,e){return(0,r.uX)(),(0,r.CE)("div",null,e[0]||(e[0]=[(0,r.Lk)("h1",null,"Welcome to CoinDS",-1),(0,r.Lk)("p",null,"This is the homepage.",-1)]))}var c=u(1241);const i={},l=(0,c.A)(i,[["render",t]]);var o=l}}]);
+//# sourceMappingURL=724.52d5a5a3.js.map

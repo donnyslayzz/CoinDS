cd685537f4138ea10d7755ffd26b7eff285739a9
donnyslayzz <81536608+donnyslayzz@users.noreply.github.com>
2025-03-06 06:34:07 +0000

    Deploying to gh-pages from @ donnyslayzz/CoinDS@0c93066b751bd845af8261386858e2b5e93ee160 🚀

--- main_branch_files/src/main.js
+++
@@ -1,8 +0,0 @@
-import "@/assets/global.css";
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Import the router
-
-const app = createApp(App);
-app.use(router); // Use Vue Router
-app.mount('#app');
--- src/views/WalletView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Wallet</h1>
-    <p>Manage your crypto wallet here.</p>
-  </div>
-</template>

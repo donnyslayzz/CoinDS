d4fbcd5c8cc9ae151953730de331efc96474fe70
Donovan Sanchez <81536608+donnyslayzz@users.noreply.github.com>
2025-03-06 01:01:36 -0800

    Delete main_branch_files directory

--- main_branch_files/src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>About CoinDS</h1>
-    <p>Learn more about our platform.</p>
-  </div>
-</template>
--- main_branch_files/src/views/TradeView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Trade</h1>
-    <p>Welcome to the trading page.</p>
-  </div>
-</template>
